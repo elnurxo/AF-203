cbd910bb3e06c2ab9f7bfaf00f3d108a69dbd439
elnurxo <elnurykh@code.edu.az>
2023-05-02 19:58:09 +0400

    fetching sorted by name bug fix

--- 19 Promise, async await, Fetch/fetch/script.js
+++ 19 Promise, async await, Fetch/fetch/script.js
@@ -13,8 +13,6 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .then((data)=>{
     if (data.statusCode>=200 && data.statusCode<300) {
         loading.style.display = "none";
-        console.log(data);
-        console.log(data);
         data.result.then((data)=>{
             data.forEach(item => {
                 list.innerHTML += `<li id="${item.id}"><a href="detail.html?id=${item.id}">${item.name}</a> | ${item.address.city}</li>`
@@ -22,7 +20,6 @@ fetch("https://jsonplaceholder.typicode.com/users")
         })
     }
     else{
-        console.log(data.statusCode);
         throw new Error("404 error not found!")
     }
 })
@@ -39,15 +36,12 @@ searchInput.addEventListener("keyup",(e)=>{
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(res=>res.json())
     .then(data=>{
+        list.innerHTML = "";
         let searhcedUsers =  data.filter(item => item.name.toLowerCase().trim().includes(e.target.value.toLowerCase().trim()));
         searhcedUsers.forEach((item)=>{
-            list.innerHTML = "";
-            list.innerHTML += `<li>${item.name} | ${item.address.city}</li>`
+            list.innerHTML += `<li><a href="detail.html?id=${item.id}">${item.name}</a> | ${item.address.city}</li>`
         })
     });
-    if (e.target.value.trim()=="") {
-        getUsers();
-    }
 });
 
 let sortBtn = document.querySelector("#sort");
@@ -56,14 +50,9 @@ sortBtn.addEventListener("click",()=>{
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(res=>res.json())
     .then(data=>{
-        let sortedArr = [];
-        data.sort((x,y)=>{
-            if (x.name<y.name) {
-               sortedArr.push(x);
-            }
-        });
-        sortedArr.forEach((x)=>{
-            list.innerHTML += `<li>${x.name} | ${x.address.city}</li>`
+        data.sort((x,y)=>x.name.localeCompare(y.name));
+        data.forEach((x)=>{
+            list.innerHTML += `<li><a href="detail.html?id=${x.id}">${x.name}</a> | ${x.address.city}</li>`
         })
     })
 })
